50fa11445612a2049b98c3a95dddbf53a0e2e341
Assembly Consultoria <fabio_carvalho242@outlook.com>
2025-05-04 21:15:16 -0300

    Criação do projeto Spring Boot com Maven, incluindo arquivos de configuração, classes principais e dependências.

---
+++ src/main/java/assemblyconsultoria/bater_ponto/BaterPontoApplication.java
@@ -0,0 +1,13 @@
+package assemblyconsultoria.bater_ponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaterPontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaterPontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assemblyconsultoria/bater_ponto/BaterPontoApplicationTests.java
@@ -0,0 +1,13 @@
+package assemblyconsultoria.bater_ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaterPontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
